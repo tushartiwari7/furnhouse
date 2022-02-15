570225850fb94915f47ac4330940ce21fbf99d16
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-02-15 14:14:25 +0530

    bug-fix: hide close icon on sidebar in desktop view.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("hello");
 const sidebar = document.getElementById("sidebar");
+// const closeBtn = document.getElementById("btn-close");
 
 const openSidebar = () => sidebar.style.left = '0';
 const closeSidebar = () => sidebar.style.left = '-25rem';
